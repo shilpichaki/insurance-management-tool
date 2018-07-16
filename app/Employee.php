7af8feeb58c85f7b14281b1797c697c5dfeb2e86
dwipraj <dwipraj.1995@gmail.com>
2018-07-16 19:04:31 +0530

    RCDK 2nd Commit Custom User and Employee Models

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    Protected $table = "tbl_employee_mast";
+    public $primaryKey = "emp_id";
+    public $timestamps = true;
+    protected $fillable = [
+        'emp_id', 'emp_first_name', 'emp_middle_name','emp_last_name','emp_dob','emp_email','emp_phno','emp_desg_id','emp_reports_to','emp_status',
+    ];
 }
--- app/Http/Controllers/Api/PassportController.php
+++ app/Http/Controllers/Api/PassportController.php
@@ -10,7 +10,39 @@ use Validator;
 
 class PassportController extends Controllers
 {
-    
+    public $successStatus = 200;
+
+    public function login()
+    {
+        if(Auth::attempt(['email' => request('email'),'password' => request('password')]))
+        {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('MyApp')->accessToken;
+            return response()->json(['success' => $success], $this->successStatus);
+        }
+        else
+        {
+            return response()->json(['error' => 'Unauthorised'], 401);
+
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), 
+            ['name' => 'required','email' => 'required'],
+            ['title.required' => 'Title is required','body.required' => 'Body is required']
+        );
+
+        if ($validator->fails()) {
+            $failedRules = $validator->failed();
+            $messages = $validator->errors()->getMessages();
+            foreach($messages as $message)
+            {
+                echo $message[0];
+            }
+        }
+    }
 }
 
 ?>
