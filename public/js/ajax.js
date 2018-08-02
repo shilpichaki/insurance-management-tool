0cb2a39ba63a074d557569d25012d81b6727ed79
dwipraj <dwipraj.1995@gmail.com>
2018-08-02 21:27:37 +0530

    Order statement frontend ajax complete

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,4 +1,15 @@
 $('#company_type').change(function(){ 
     var value = $(this).val();
-    alert(value);
+    switch(value)
+    {
+        case "mother":
+            $('#s_company_id').parents('.form-group').hide();
+        break;
+        case "sub":
+            $('#s_company_id').parents('.form-group').show();
+        break;
+        default:
+            $('#s_company_id').parents('.form-group').show();
+    }
+    
 });
--- resources/views/orderstatement/showform.blade.php
+++ resources/views/orderstatement/showform.blade.php
@@ -20,6 +20,20 @@
             </div>
         </div>
     
+        <div class="form-group{{ $errors->has('s_company_id') ? ' has-error' : '' }}">
+            <label for="s_company_id" class="col-md-4 control-label">Sub Company</label>
+
+            <div class="col-md-6">
+
+                <select id="s_company_id" class="form-control" name="s_company_id" autofocus>
+                    <option value = "">-Please Select One-</option>
+                @foreach ($subcompanylist as $subcompany)
+                    <option value="{{ $subcompany->s_company_id }}">{{ $subcompany->s_company_name }}</option>
+                @endforeach
+                </select>
+            </div>
+        </div>
+    
         <div class="form-group{{ $errors->has('m_company_id') ? ' has-error' : '' }}">
             <label for="m_company_id" class="col-md-4 control-label">Mother Company</label>
     
@@ -29,20 +43,6 @@
                     <option value = "">-Please Select One-</option>
                 @foreach ($mothercompanylist as $mothercompany)
                     <option value="{{ $mothercompany->m_company_id }}">{{ $mothercompany->m_company_name }}</option>
-                @endforeach
-                </select>
-            </div>
-        </div>
-    
-        <div class="form-group{{ $errors->has('s_company_id') ? ' has-error' : '' }}">
-            <label for="s_company_id" class="col-md-4 control-label">Sub Company</label>
-    
-            <div class="col-md-6">
-    
-                <select id="s_company_id" class="form-control" name="s_company_id" autofocus>
-                    <option value = "">-Please Select One-</option>
-                @foreach ($subcompanylist as $subcompany)
-                    <option value="{{ $subcompany->s_company_id }}">{{ $subcompany->s_company_name }}</option>
                 @endforeach
                 </select>
             </div>
